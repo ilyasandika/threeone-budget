890412ff3247c58e7410871bcf4900b5f271658b
Ilyas Andika <andikailyas01@gmail.com>
2024-10-15 14:20:33 +0700

    misc: move icons to src/assets/icons

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    return (
+	<div>
+	    <span>Thursday, 20 June 2024, 11:41 WIB</span>
+	    <span>Ilyas Andika</span>
+	    <span>Admin Keuagan</span>
+
+	</div>
+    )
+}
+
+export default Header
