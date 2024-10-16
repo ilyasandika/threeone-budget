a5915e2902a4c694a9db4f758889a8d5aa21710d
Ilyas Andika <andikailyas01@gmail.com>
2024-10-16 13:24:01 +0700

    feat: add active icons

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -12,7 +12,7 @@ interface NavItemProps {
 const NavItem: React.FC<NavItemProps> = ({to, activeImage, image, title, active }) => {
     return (
 	<Link to={to} className={`flex items-center px-4 py-2 cursor-pointer rounded-lg ${active ? "bg-sky-950 text-white" : "hover:bg-sky-950 hover:bg-opacity-5"}`}>
-	    <img alt="" src={active ? activeImage : image} className={`inline mr-4`}/>
+	    <img alt="" src={active ? activeImage : image} className={`inline w-5 mr-4`}/>
 	    <span className="font-medium">{title}</span>
 	</Link>
     );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,18 +6,30 @@ import entry from "../../assets/icons/entry.svg";
 import report from "../../assets/icons/report.svg";
 import setting from "../../assets/icons/setting.svg";
 import account from "../../assets/icons/account.svg";
+import {useLocation} from "react-router-dom";
 
-const Navbar = () => (
-    <div className="fixed top-0 left-0 text-left w-72 bg-white h-screen p-8 border-r z-10">
-	<img alt="" src={mainLogo} className="mx-auto mb-14 w-64"/>
-	<div className="flex flex-col gap-3">
-	    <NavItem to="/" activeImage={dashboardWhite} image={dashboard} title="Dashboard" active={true}></NavItem>
-	    <NavItem to="/entry" activeImage={dashboardWhite} image={entry} title="Entry"></NavItem>
-	    <NavItem to="/report" activeImage={dashboardWhite} image={report} title="Report"></NavItem>
-	    <NavItem to="/setting" activeImage={dashboardWhite} image={setting} title="Setting"></NavItem>
-	    <NavItem to="/account" activeImage={dashboardWhite} image={account} title="Account"></NavItem>
+
+
+const Navbar = () => {
+    const location = useLocation();
+    const firstPath = location.pathname.split('/').filter(Boolean)[0];
+
+    const isActive = (path: string) => {
+	return firstPath == path;
+    }
+
+    return (
+	<div className="fixed top-0 left-0 text-left w-72 bg-white h-screen p-8 border-r z-10">
+	    <img alt="" src={mainLogo} className="mx-auto mb-14 w-64"/>
+	    <div className="flex flex-col gap-3">
+		<NavItem to="/dashboard" activeImage={dashboardWhite} image={dashboard} title="Dashboard" active={isActive("dashboard")}/>
+		<NavItem to="/entry" activeImage={dashboardWhite} image={entry} title="Entry" active={isActive("entry")}/>
+		<NavItem to="/report" activeImage={dashboardWhite} image={report} title="Report" active={isActive("report")}/>
+		<NavItem to="/setting" activeImage={dashboardWhite} image={setting} title="Setting" active={isActive("setting")}/>
+		<NavItem to="/account" activeImage={dashboardWhite} image={account} title="Account" active={isActive("account")}/>
+	    </div>
 	</div>
-    </div>
-)
+    )
+}
 
 export default Navbar
